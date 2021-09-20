28e47d794566e2d0fa1a510b32ad9b5dbb85c8cb
Renato  Rocha <renato@Renatos-iMac.local>
2021-09-19 22:08:51 -0300

    (WIP) - Add images to mi card

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -2,11 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/avatar.jpg"),
+              ),
+              Text(
+                "Maria Carla",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "Source Sans Pro",
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
